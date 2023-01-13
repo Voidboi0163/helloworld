0beaec28ea57058dbd0d7e7503004461c71db82e
Voidboi0163 <122555118+Voidboi0163@users.noreply.github.com>
2023-01-13 12:05:15 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,9 @@
+def on_button_pressed_a():
+    pass
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    pass
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+basic.show_icon(IconNames.SMALL_SQUARE)
